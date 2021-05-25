450428b4577cc7cb08e2201717a21cf132a15710
joaolobao380 <victor_jv380@hotmail.com>
2021-05-25 00:40:26 -0300

    added plop

---
+++ generators/plopfile.js
@@ -0,0 +1,35 @@
+module.exports = function (plop) {
+  // create your generators here
+  plop.setGenerator('component', {
+    description: 'this is a skeleton plopfile',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name please',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.test.tsx',
+        templateFile: 'templates/index.test.tsx.hbs',
+      },
+    ],
+  });
+};
--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,6 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: ['src/**/*.ts(x)', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-  // moduleNameMapper: {
-  //   'ˆstyled-components':
-  //     '<rootDir> /node_modules/styled-components/dist/styled-components.browser.cjs.js',
-  // },
 };
